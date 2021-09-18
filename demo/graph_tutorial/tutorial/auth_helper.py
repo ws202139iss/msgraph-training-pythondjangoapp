ea45b2b82745ac22d5c7bfa80ce09fe855ed37ed
AksenovAV <26maniak@gmail.com>
2021-09-18 12:32:44 +0700

    change env var name

--- demo/graph_tutorial/tutorial/auth_helper.py
+++ demo/graph_tutorial/tutorial/auth_helper.py
@@ -40,7 +40,7 @@ def get_sign_in_flow():
 
   return auth_app.initiate_auth_code_flow(
     settings['scopes'],
-    redirect_uri=os.getenv('REDIRECT_URL'))
+    redirect_uri=os.getenv('REDIRECT'))
 
 # Method to exchange auth code for access token
 def get_token_from_code(request):
